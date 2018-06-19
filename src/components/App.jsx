7a33e5ed8ae7b0fe7f537a5db9fa5f7c021e30dd
HugoCapitan <hmoravilez@gmail.com>
2018-06-19 15:59:14 -0500

    Fixing styles yeye

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import { withStyles } from '@material-ui/core/styles'
+
+import GoalList from './GoalList'
 import TopBar from './TopBar'
 
-export default () => (
-  <div>
+const styles = {
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1
+  }
+}
+
+const App = ({ classes }) => (
+  <div className={ classes.root } >
     <TopBar />
-    Hello ppl.
+    <GoalList className={ classes.content } />
   </div>
 )
+
+export default withStyles(styles)(App)
