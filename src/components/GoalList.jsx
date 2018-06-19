8e326006216afd7183c0fd06653ef42f4276a5e0
HugoCapitan <hmoravilez@gmail.com>
2018-06-19 15:58:53 -0500

    Starting goallist component

---
+++ src/components/GoalList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+import AddGoalTooltip from './AddGoalTooltip'
+
+const styles = {
+  root: {
+    marginTop: '56px',
+    padding: '8px'
+  },
+  nothingMessage: {
+    position: 'absolute',
+    transform: 'translate(-50%, -50%)',
+    top: '50%',
+    left: '50%',
+    width: 'fit-content'
+  }
+}
+
+const GoalList = ({ classes }) => (
+  <div className={ classes.root }>
+    <Typography className={ classes.nothingMessage } color="textSecondary" variant="headline"> 
+      You have no goals yet!
+    </Typography>
+    <AddGoalTooltip />
+  </div>
+)
+
+export default withStyles(styles)(GoalList)
