67afd2d47fd7a293b46d289c68d6dbb943a37550
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-06-27 20:08:45 -0500

    Files for Habit and Goal Detailed Pages

---
+++ src/components/HabitDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+})
+
+const HabitDetail = ({ classes }) => (
+  <div> Habit Detail </div>
+)
+
+export default withStyles(styles)(HabitDetail)
+
