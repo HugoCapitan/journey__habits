79fbab2d1c9023458f697fa33d01bb75b6a0065b
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-06-27 20:31:46 -0500

    Root styles for habitdetail and new file for habitdateline

---
+++ src/components/HabitDateLine.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+})
+
+const HabitDateLine = ({ classes }) => (
+  <div>
+    HabitDateLine
+  </div>
+)
+
+export default withStyles(styles)(HabitDateLine)
+
--- src/components/HabitDetail.jsx
+++ src/components/HabitDetail.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+
 import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+import HabitDateLine from './HabitDateline'
 
 const styles = theme => ({
+  root: {
+    marginTop: '56px',
+    overflow: 'scroll',
+    padding: '8px',
+    paddingBottom: '80px'
+  },
 })
 
 const HabitDetail = ({ classes }) => (
-  <div> Habit Detail </div>
+  <div className={ classes.root }>
+    <HabitDateLine />
+  </div>
 )
 
 export default withStyles(styles)(HabitDetail)
