6b6b37d52ce66915dc2ba043a872315ac14e02fd
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-06-28 20:08:47 -0500

    Getting days of the week and adding pretty styles to them

--- src/components/HabitDateLine.jsx
+++ src/components/HabitDateLine.jsx
@@ -1,15 +1,43 @@
+import moment from 'moment'
 import React from 'react'
 
 import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
 const styles = theme => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+  },
+  dayContainer: {
+    flexGrow: 1,
+  },
+  day: {
+    border: '2px solid',
+    borderColor: theme.palette.primary.light,
+    borderRadius: '1000px',
+    display: 'block',
+    height: 0,
+    padding: '20% 0 60%',
+    textAlign: 'center',
+    width: '80%',
+  }
 })
 
-const HabitDateLine = ({ classes }) => (
-  <div>
-    HabitDateLine
-  </div>
-)
+const HabitDateLine = ({ classes }) => {
+  // Getting days
+  const weekDates = [1,2,3,4,5,6,0].map(item => moment().day(item).format('D'))
+
+  return (
+    <div className={ classes.root } >
+      { weekDates.map((day, index) => (
+        <div className={ classes.dayContainer } key={index}> 
+          <Typography color="inherit" className={ classes.day }> { day } </Typography>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default withStyles(styles)(HabitDateLine)
 
